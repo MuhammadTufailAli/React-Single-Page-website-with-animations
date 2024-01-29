728ecab16f0e69a6063001eefa70e019fe332d0e
MuhammadTufail1 <muhammad.tufail@nayatel.com>
2024-01-29 10:42:53 +0500

    NavBar done

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   Container,
   Menu,
@@ -7,12 +7,11 @@ import {
   Button,
   Modal,
   Header,
-} from "semantic-ui-react";
-import logoImage from "../assets/logo1.png"; // Import your logo image
-import toggle from "../assets/toggle.png"; // Import your logo image
-import cross from "../assets/cross1.png";
-import "../css/AppHeader.css";
-
+} from 'semantic-ui-react';
+import logoImage from '../assets/logo1.png'; // Import your logo image
+import toggle from '../assets/toggle.png'; // Import your logo image
+import cross from '../assets/cross1.png';
+import '../css/AppHeader.css';
 
 function NavBar() {
   const [isMobile, setIsMobile] = useState(false);
@@ -24,39 +23,41 @@ function NavBar() {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
+      setIsMobile(window.innerWidth <= 1146);
       if (!isMobile && modalOpen) {
         setModalOpen(false);
       }
     };
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
     handleResize(); // Initial check
 
-    return () => window.removeEventListener("resize", handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, [isMobile, modalOpen]);
 
   return (
     <div>
       <Menu
-        fixed="top"
+        // fixed='top'
         inverted
         stackable
         borderless
         style={{
-          justifyContent: "center",
-          alignSelf: "center",
-          alignItems: "center",
-          display: "flex",
+          justifyContent: 'center',
+          marginTop: '-136px',
+          alignSelf: 'center',
+          alignItems: 'center',
+          display: 'flex',
         }}
       >
         <Container
+          className='AppHeaderTopContainer'
           style={{
-            width: "95%",
-            display: "flex",
-            alignItems: "center",
-            backgroundColor: "#353535",
-            height: "96px",
+            width: '95%',
+            display: 'flex',
+            alignItems: 'center',
+            backgroundColor: '#353535',
+            height: '96px',
             borderRadius: 60,
             marginTop: 40,
           }}
@@ -64,68 +65,92 @@ function NavBar() {
           <Menu.Item
             header
             style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
+              display: 'flex',
+
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingLeft: 32,
             }}
           >
             <Image
               style={{
-                width: 40,
-                height: 40,
-                paddingLeft: 40,
+                width: 64,
+
                 paddingRight: 20,
               }}
               src={logoImage}
-              size="tiny"
+              size='tiny'
             />
           </Menu.Item>
           <Menu.Item
-            as="a"
-            style={{ color: "white", marginRight: 20 }}
-            className={isMobile ? "menu-toggle" : ""}
+            as='a'
+            style={{ color: 'white', marginRight: 20 }}
+            className={isMobile ? 'menu-toggle' : ''}
           >
-            Home
+            Creators
           </Menu.Item>
           <Menu.Item
-            as="a"
-            style={{ color: "white", marginRight: 20 }}
-            className={isMobile ? "menu-toggle" : ""}
+            as='a'
+            style={{ color: 'white', marginRight: 20 }}
+            className={isMobile ? 'menu-toggle' : ''}
           >
-            About
+            Copiers
           </Menu.Item>
           <Menu.Item
-            as="a"
-            style={{ color: "white", marginRight: 20 }}
-            className={isMobile ? "menu-toggle" : ""}
+            as='a'
+            style={{ color: 'white', marginRight: 20 }}
+            className={isMobile ? 'menu-toggle' : ''}
           >
-            Services
+            Live Chart
           </Menu.Item>
           <Menu.Item
-            as="a"
-            style={{ color: "white", marginRight: 20 }}
-            className={isMobile ? "menu-toggle" : ""}
+            as='a'
+            style={{ color: 'white', marginRight: 20 }}
+            className={isMobile ? 'menu-toggle' : ''}
           >
-            Contact
+            Marketplace
+          </Menu.Item>
+
+          <Menu.Item
+            as='a'
+            style={{ color: 'white', marginRight: 20 }}
+            className={isMobile ? 'menu-toggle' : ''}
+          >
+            Pricing
+          </Menu.Item>
+          <Menu.Item
+            as='a'
+            style={{ color: 'white', marginRight: 20 }}
+            className={isMobile ? 'menu-toggle' : ''}
+          >
+            Content Hub
+          </Menu.Item>
+
+          <Menu.Item
+            as='a'
+            style={{ color: 'white', marginRight: 20 }}
+            className={isMobile ? 'menu-toggle' : ''}
+          >
+            Documentation
           </Menu.Item>
 
           {isMobile ? (
             <Button
               style={{
-                backgroundColor: "black",
+                backgroundColor: 'black',
                 width: 100,
                 height: 35,
                 borderRadius: 20,
-                color: "white",
+                color: 'white',
                 borderWidth: 1,
-                display: "flex",
-                marginLeft: "auto",
-                justifyContent: "center",
-                alignItems: "center",
-                background: "none",
-                border: "none",
+                display: 'flex',
+                marginLeft: 'auto',
+                justifyContent: 'center',
+                alignItems: 'center',
+                background: 'none',
+                border: 'none',
                 padding: 0,
-                cursor: "pointer",
+                cursor: 'pointer',
               }}
               icon
               onClick={toggleModal}
@@ -133,27 +158,27 @@ function NavBar() {
               <Image
                 src={toggle}
                 style={{ width: 30, height: 20 }}
-                size="mini"
+                size='mini'
               />
             </Button>
           ) : (
             <Button
               primary
               style={{
-                backgroundColor: "black",
+                backgroundColor: 'black',
                 width: 100,
                 height: 35,
                 borderRadius: 20,
-                color: "white",
+                color: 'white',
                 borderWidth: 1,
-                display: "flex",
-                marginLeft: "auto",
+                display: 'flex',
+                marginLeft: 'auto',
                 marginRight: 30,
-                justifyContent: "center",
-                alignItems: "center",
-                border: "none",
+                justifyContent: 'center',
+                alignItems: 'center',
+                border: 'none',
                 padding: 0,
-                cursor: "pointer",
+                cursor: 'pointer',
               }}
             >
               Login
@@ -165,112 +190,109 @@ function NavBar() {
         open={modalOpen}
         onClose={toggleModal}
         style={{
-          backgroundColor: "#353535",
-          textAlign: "center",
-          width: "80%",
+          backgroundColor: '#353535',
+          textAlign: 'center',
+          width: '80%',
           borderRadius: 30,
           marginBottom: 5,
           marginTop: 5,
-          alignSelf: "center",
-          marginLeft: "auto",
-          marginRight: "auto",
-          display: "flex",
-          alignItems: "flex-start",
-          flexDirection: "column",
+          alignSelf: 'center',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          display: 'flex',
+          alignItems: 'flex-start',
+          flexDirection: 'column',
 
           padding: 30,
         }}
       >
         <Button
           style={{
-            backgroundColor: "black",
+            backgroundColor: 'black',
             width: 40,
             height: 40,
             borderRadius: 20,
 
             borderWidth: 1,
-            display: "flex",
-            marginLeft: "auto",
-            justifyContent: "center",
-            alignItems: "center",
-            background: "white",
-            border: "none",
+            display: 'flex',
+            marginLeft: 'auto',
+            justifyContent: 'center',
+            alignItems: 'center',
+            background: 'white',
+            border: 'none',
             padding: 0,
-            cursor: "pointer",
+            cursor: 'pointer',
           }}
           icon
           onClick={toggleModal}
         >
-          <Image src={cross} style={{ width: 15, height: 15 }} size="mini" />
+          <Image
+            src={cross}
+            style={{ width: 15, height: 15, marginTop: 0 }}
+            size='mini'
+          />
         </Button>
-        <Menu.Item style={{ marginTop: 30, color: "white" }} as="a">
-          Creators
-        </Menu.Item>
-        <Menu.Item style={{ marginTop: 30, color: "white" }} as="a">
-          Copiers
-        </Menu.Item>
-        <Menu.Item style={{ marginTop: 30, color: "white" }} as="a">
-          Live Chart
-        </Menu.Item>
-        <Menu.Item style={{ marginTop: 30, color: "white" }} as="a">
-          Marketplace
-        </Menu.Item>
-        <Menu.Item style={{ marginTop: 30, color: "white" }} as="a">
-          Pricing
-        </Menu.Item>
-        <Menu.Item style={{ marginTop: 30, color: "white" }} as="a">
-          Content Hub
-        </Menu.Item>
-
-        <Menu.Item style={{ marginTop: 30, color: "white" }} as="a">
-          Documentation
-        </Menu.Item>
         <div
           style={{
-            width: "90%",
-            borderWidth: 1,
-            borderStyle: "double",
-            color: "white",
-            display: "flex",
-            alignItems: "center",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'start',
+          }}
+        >
+          <Menu.Item style={{ marginTop: 30, color: 'white' }} as='a'>
+            Creators
+          </Menu.Item>
+          <Menu.Item style={{ marginTop: 30, color: 'white' }} as='a'>
+            Copiers
+          </Menu.Item>
+          <Menu.Item style={{ marginTop: 30, color: 'white' }} as='a'>
+            Live Chart
+          </Menu.Item>
+          <Menu.Item style={{ marginTop: 30, color: 'white' }} as='a'>
+            Marketplace
+          </Menu.Item>
+          <Menu.Item style={{ marginTop: 30, color: 'white' }} as='a'>
+            Pricing
+          </Menu.Item>
+          <Menu.Item style={{ marginTop: 30, color: 'white' }} as='a'>
+            Content Hub
+          </Menu.Item>
+
+          <Menu.Item style={{ marginTop: 30, color: 'white' }} as='a'>
+            Documentation
+          </Menu.Item>
+        </div>
+        <div
+          style={{
+            width: '90%',
+            // borderWidth: 1,
+            // borderStyle: 'double',
+            color: 'white',
+            display: 'flex',
+            alignItems: 'center',
             marginTop: 25,
           }}
         ></div>
-        <Menu.Item style={{ marginTop: 20, color: "white" }} as="a">
-          Login
-        </Menu.Item>
-        <div
-          style={{
-            width: "90%",
-            borderWidth: 1,
-            borderStyle: "double",
-            color: "white",
-            display: "flex",
-            alignItems: "center",
-            marginTop: 20,
-          }}
-        ></div>
-        <Button
-          primary
-          style={{
-            backgroundColor: "#FFEB92",
-            width: 200,
-            height: 35,
-            borderRadius: 20,
-            color: "black",
-            borderWidth: 1,
-            display: "flex",
-            marginTop: 40,
 
-            justifyContent: "center",
-            alignItems: "center",
-            alignSelf: "center",
-            marginLeft: "auto",
-            marginRight: "auto",
-            border: "none",
-            padding: 0,
-            cursor: "pointer",
-          }}
+        <div
+          className='LoginModelButton'
+          // style={{
+          //   width: '90%',
+          //   borderWidth: 1,
+          //   borderStyle: 'double',
+          //   color: 'white',
+          //   display: 'flex',
+          //   alignItems: 'center',
+          //   marginTop: 20,
+          // }}
+        >
+          <Menu.Item style={{ marginTop: 20, color: 'white' }} as='a'>
+            Login
+          </Menu.Item>
+        </div>
+        <Button
+          style={{ marginTop: 40 }}
+          className='Section1TopDivJoinUsButton'
         >
           Join Us
         </Button>
